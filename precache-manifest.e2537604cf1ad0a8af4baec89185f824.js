3e26e88285aa61ed07dcc2d8888de7678102d6e5
Logan Bateman <lbateman0199@gmail.com>
2020-09-20 19:26:32 -0400

    Updates

---
+++ precache-manifest.e2537604cf1ad0a8af4baec89185f824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580835326f789aea18fe8ac37c091349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80686c0d63689bf403ca",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3264c7b76fb87cd8dfab",
+    "url": "/static/js/2.41f85708.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41f85708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80686c0d63689bf403ca",
+    "url": "/static/js/main.2b0d4ef6.chunk.js"
+  },
+  {
+    "revision": "29b03411a41635529420",
+    "url": "/static/js/runtime-main.f32b7799.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
