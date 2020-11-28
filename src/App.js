22216c19df01f8189c47f9f552b6966fdb9a814b
Logan Bateman <lbateman0199@gmail.com>
2020-11-28 00:27:11 -0500

    Added new LockContainer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LockContainer from "./Components/LockContainer";
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [password, setPassword] = useState("password123");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(Date.now().toString())
+    }, 100)
+  });
+
+  setTimeout(() => {
+    
+  })
+
+  const handlePassChange = (evt) => {
+    setPassword(evt.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>nametable's website</p>
-        <p>Your computer says the time is {Date.now().toString()}</p>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
+        <p>Cur Pass <input onChange={handlePassChange} value={password}></input></p>
+        <LockContainer password={password} height={500} width={500} color="black">
+          <div style={{display:"grid", gridTemplateColumns: "auto auto"}}>
+            <LockContainer password={password} height={250} width={250} color="red"></LockContainer>
+            <LockContainer password={password} height={250} width={250} color="green"></LockContainer>
+            <LockContainer password={password} height={250} width={250} color="blue"></LockContainer>
+            <LockContainer password={password} height={250} width={250} color="#123456">
+              <iframe src="https://www.youtube.com/embed/hDkuUZ3F1GU" frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen></iframe>
+            </LockContainer>
+          </div>
+        </LockContainer>
+        <p>nametable's website - Very much a work in progress</p>
+        <p>Your computer says the time is {time}</p>
       </header>
     </div>
   );
---
+++ src/Components/LockContainer.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react';
+import './LockContainer.css';
+
+function LockContainer(props) {
+
+    const containerStyle = {
+        width: props.width,
+        height: props.height
+    };
+    let overlayStyleTop = () => {
+        return (
+            {
+                // width: isLocked() ? props.width : 0,
+                // height: isLocked() ? props.height / 2 : 0,
+                backgroundColor: props.color,
+                width: props.width,
+                height: props.height / 2,
+                top: isLocked() ? 0 : 0- props.height / 2,
+            }
+        )
+    }
+    let overlayStyleBottom = () => {
+        return (
+            {
+                // width: isLocked() ? props.width : 0,
+                // height: isLocked() ? props.height / 2 : 0,
+                backgroundColor: props.color,
+                width: props.width,
+                height: props.height / 2,
+                top: isLocked() ? props.height / 2 : props.height,
+            }
+        )
+    }
+
+    const [inputText, setInputText] = useState("");
+    let isLocked = () => {
+        return (inputText != props.password);
+    }
+
+    function handleInputChange(evt){
+        // console.log(evt.target.value);
+        setInputText(evt.target.value);
+    }
+
+    return (
+        <div id="lock-container-main" style={containerStyle}>
+            <div id="lock-container-content">
+                {/*<p>Current text is "{inputText}". Locked: {isLocked().toString()}</p>*/}
+                {props.children}
+            </div>
+            <div id="lock-div-1" style={overlayStyleTop()} className="lock-overlay">
+                <input onChange={handleInputChange} id="lock-password-input" type="text"></input>
+            </div>
+            <div id="lock-div-2" style={overlayStyleBottom()} className="lock-overlay"></div>
+        </div>
+    );
+}
+
+export default LockContainer;
